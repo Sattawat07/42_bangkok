179a1991a0974c3bdaa9d6f159355205e8774999
Pite <boontem.sprite07@gmail.com>
2026-09-09 21:39:51 +0700

    Add ft_printf.h

---
+++ ft_prtinf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboontem <sboontem@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/09 21:37:50 by sboontem          #+#    #+#             */
+/*   Updated: 2026/09/09 21:37:50 by sboontem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *f, ...);
+int	matchf(va_list args, char specifier);
+int	ft_putchr(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_puthex(unsigned int num, char specifier);
+int	ft_putptr(void *ptr);
+
+#endif
